8c977e43d56714bff90dde7688ac111b56870ab6
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-30 21:37:26 -0500

    style

--- learn-typescript/src/App.tsx
+++ learn-typescript/src/App.tsx
@@ -1,7 +1,23 @@
-function Button(props: {text: string}) {
+type Color = 'bg-yellow-500' | 'bg-blue-500' | 'bg-red-500'
+
+type ButtonProps = {
+  text: string
+  subtitle?: string
+  color?: Color
+  backgroundColor?: Color
+  padding?: number[]
+  paddingLengLimit?: [number, number, number?, number?]
+  names: ['Ángel' | 'David', 'Hurtado']
+  style: React.CSSProperties
+}
+
+
+function Button({text, backgroundColor, style}: ButtonProps) {
+  const bg = backgroundColor ? backgroundColor : 'bg-amber-500'
+
   return (
-    <button className='bg-amber-500 text-blue-950 font-semibold py-1 px-2 rounded-md'>
-      {props.text}
+    <button style={style} className={`${bg} text-blue-950 font-semibold py-1 px-2 rounded-md`}>
+      {text}
     </button>
   )
 }
@@ -10,7 +26,7 @@ function App() {
   return (
     <>
       <h1>Hola mundo</h1>
-      <Button text={'hola undo'} />
+      <Button text='Hola undo' names={['Ángel', 'Hurtado']} style={{fontSize: '32px'}} />
     </>
   )
 }
