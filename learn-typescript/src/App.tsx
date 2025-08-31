271e907ab3da1338864b141e7f5801af92326d7d
Ángel David Hurtado <angeldavidhurtado.dev@gmail.com>
2025-08-30 19:46:13 -0500

    type

--- learn-typescript/src/App.tsx
+++ learn-typescript/src/App.tsx
@@ -1,7 +1,16 @@
+function Button(props: {text: string}) {
+  return (
+    <button className='bg-amber-500 text-blue-950 font-semibold py-1 px-2 rounded-md'>
+      {props.text}
+    </button>
+  )
+}
+
 function App() {
   return (
     <>
       <h1>Hola mundo</h1>
+      <Button text={'hola undo'} />
     </>
   )
 }
